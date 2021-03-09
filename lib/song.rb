17933b2c69cbafa858331dc8b53c281e945fb5f3
Jesse Wenger <wengerj@gmail.com>
2021-03-09 01:06:17 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song 
+  @@all = []
+  attr_accessor :artist, :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def artist_name
+    if self.artist != nil 
+      self.artist.name
+    else
+      nil
+    end
+  end
+end
